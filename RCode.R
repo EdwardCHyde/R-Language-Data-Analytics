d256b1e5cca677b7f76d5a196f29a543ca94dd86
Collin Hyde <106849612+EdwardCHyde@users.noreply.github.com>
2022-11-09 11:17:21 -0700

    Add files via upload

---
+++ RCode.R
@@ -0,0 +1,41 @@
+install.packages("gapminder")
+library(gapminder)
+data("gapminder")
+
+summary(gapminder)
+x = mean(gapminder$gdpPercap)
+x
+
+attach(gapminder)
+median(pop)
+hist(lifeExp)
+hist(log(pop))
+boxplot((lifeExp ~ continent))
+plot(lifeExp ~ log(gdpPercap))
+
+install.packages("dplyr")
+library(dplyr)
+
+df1 = gapminder %>%
+  select(country, lifeExp) %>%
+  filter(country == "United Kingdom" | 
+           country == "United States")
+
+t.test(data = df1, lifeExp ~ country)
+
+install.packages("ggplot2")
+library(ggplot2)  
+
+gapminder %>%
+  filter(gdpPercap < 50000) %>%
+  ggplot(aes(x=log(gdpPercap), y=lifeExp, col=year, size=pop))+
+  geom_point(alpha=0.3)+
+  geom_smooth(method = lm)+
+  facet_wrap(~continent)
+
+summary(lm(lifeExp ~ gdpPercap)
+
+
+
+
+  
